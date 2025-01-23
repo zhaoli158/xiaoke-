c00cb368fe92a298d97083559f8882b3fc69e2c0
赵李彦宇 <111413175+zhaoli158@users.noreply.github.com>
2025-01-23 11:30:49 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'xiaoke-cache-v1';
+const urlsToCache = [
+    '/小可查询.html',
+    '/manifest.json',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+}); 
